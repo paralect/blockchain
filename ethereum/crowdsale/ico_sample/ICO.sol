0c45a0ab7fa960038c5b35e4b64836c070aecfaa
Gokhan Simsek <g.simsek@paralect.com>
2018-06-13 11:39:55 +0300

    Adding features: lock purchased tokens, withdrawTokens after ICO ends and withdrawFunds after ICO.

--- ethereum/crowdsale/ico_sample/ICO.sol
+++ ethereum/crowdsale/ico_sample/ICO.sol
@@ -10,11 +10,11 @@ contract Crowdsale {
     uint public fundingGoal;
     uint public amountRaised;
     uint public deadline;
-    uint public price;
+    uint public tokensPerWei;
     token public tokenReward;
-    mapping(address => uint256) public balanceOf;   // eth balance of donors
-    bool public fundingGoalReached = false;
-    bool public crowdsaleClosed = false;
+    mapping(address => uint256) public tokenBalanceOf;   // token balance of donors
+    bool fundingGoalReached = false;
+    bool crowdsaleClosed = false;
 
     event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
@@ -28,13 +28,13 @@ contract Crowdsale {
         address ifSuccessfulSendTo,
         uint fundingGoalInEthers,
         uint durationInMinutes,
-        uint finneyCostOfEachToken,
+        uint tokensForOneWei,
         address addressOfTokenUsedAsReward
     ) public {
         beneficiary = ifSuccessfulSendTo;
         fundingGoal = fundingGoalInEthers * 1 ether;        // 1 ether == 1,000,000,000,000,000,000
         deadline = now + durationInMinutes * 1 minutes;
-        price = finneyCostOfEachToken * 1 finney;           // 1 ether === 1000 finney 
+        tokensPerWei = tokensForOneWei;      // 1 wei -> 1000 tokens for now (0.001 eth == 1x10^18 tokens)
         tokenReward = token(addressOfTokenUsedAsReward);    // instantiate a contract at a given address
     }
 
@@ -46,13 +46,13 @@ contract Crowdsale {
     function () payable public {
         require(!crowdsaleClosed);
         uint amount = msg.value;
-        balanceOf[msg.sender] += amount;
+        tokenBalanceOf[msg.sender] += amount * tokensPerWei;
         amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount / price);
-        emit FundTransfer(msg.sender, amount, true);
     }
 
-    modifier afterDeadline() { if (now >= deadline) _; }
+    modifier afterDeadline() { 
+        if (now >= deadline) _; 
+    }
 
     /**
      * Check if goal was reached
@@ -60,13 +60,30 @@ contract Crowdsale {
      * Checks if the goal or time limit has been reached and ends the campaign
      */
     function checkGoalReached() afterDeadline public {
-        if (amountRaised >= fundingGoal){
-            fundingGoalReached = true;
-            emit GoalReached(beneficiary, amountRaised);
-        }
+        // if (amountRaised >= fundingGoal){
+        //     fundingGoalReached = true;
+        //     emit GoalReached(beneficiary, amountRaised);
+        // }
         crowdsaleClosed = true;
     }
 
+    /**
+     * Withdraw all tokens 
+     *  - todo: support withdraw(some tokens)
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function withdrawTokens() afterDeadline public {
+        uint tokens = tokenBalanceOf[msg.sender];
+        tokenBalanceOf[msg.sender] = 0;     // fix for reentrancy bug
+        if (tokens > 0) {
+            tokenReward.transfer(msg.sender, tokens);
+            emit FundTransfer(msg.sender, tokens, true);
+        }
+        // todo: add else to assign back the tokens if token transfer fails
+    }
 
     /**
      * Withdraw the funds
@@ -75,20 +92,8 @@ contract Crowdsale {
      * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
      * the amount they contributed.
      */
-    function safeWithdrawal() afterDeadline public {
-        if (!fundingGoalReached) {
-            uint amount = balanceOf[msg.sender];
-            balanceOf[msg.sender] = 0;
-            if (amount > 0) {
-                if (msg.sender.send(amount)) {
-                    emit FundTransfer(msg.sender, amount, false);
-                } else {
-                    balanceOf[msg.sender] = amount;
-                }
-            }
-        }
-
-        if (fundingGoalReached && beneficiary == msg.sender) {
+    function withdrawFunds() afterDeadline public {
+        if (beneficiary == msg.sender) {
             if (beneficiary.send(amountRaised)) {
                 emit FundTransfer(beneficiary, amountRaised, false);
             } else {
@@ -97,4 +102,5 @@ contract Crowdsale {
             }
         }
     }
+
 }
