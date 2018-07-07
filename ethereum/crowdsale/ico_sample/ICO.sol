6fa6b8d3e07df699bf7b5e568b4a97143052d2c6
Gokhan Simsek <g.simsek@paralect.com>
2018-07-07 18:18:29 +0300

    Adding and using SafeMath library

--- ethereum/crowdsale/ico_sample/ICO.sol
+++ ethereum/crowdsale/ico_sample/ICO.sol
@@ -1,5 +1,54 @@
+pragma solidity 0.4.21;
 
-pragma solidity ^0.4.16;
+/**
+* @title SafeMath by OpenZeppelin
+* @dev Math operations with safety checks that throw on error
+*/
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
 
 interface token {
     function transfer(address receiver, uint amount) external;
@@ -10,14 +59,13 @@ interface token {
 contract Crowdsale {
     address public owner;
     address public beneficiary;
-    uint public amountRaised;
-    uint public tokensForSale;
-    uint public tokensSold;
-    uint public icoDeadline;
-    uint public tokensClaimableDeadline;
-    uint public tokensPerWei;
+    uint256 public amountRaised;
+    uint256 public tokensForSale;
+    uint256 public tokensSold;
+    uint256 public icoDeadline;
+    uint256 public tokensClaimableDeadline;
+    uint256 public tokensPerWei;
     token public tokenReward;
-    bool public crowdsaleClosed = false;
     bool public unsoldTokensBurnt = false;
     bool public unsoldTokensTransferred = false;
 
@@ -27,7 +75,7 @@ contract Crowdsale {
     // This is a type for a single Investor
     struct Inv {
         bool whitelisted;
-        uint purchasedTokens;     
+        uint256 purchasedTokens;     
         bool tokensClaimed;      
     }
 
@@ -40,9 +88,9 @@ contract Crowdsale {
      */
     function Crowdsale(
         address ifSuccessfulSendTo,
-        uint durationOfIcoInMinutes,
-        uint durationTokensClaimableAfterInMinutes,
-        uint tokensForOneWei,
+        uint256 durationOfIcoInMinutes,
+        uint256 durationTokensClaimableAfterInMinutes,
+        uint256 tokensForOneWei,
         address addressOfTokenUsedAsReward
     ) public {
         owner = msg.sender;
@@ -62,18 +110,17 @@ contract Crowdsale {
         require(now < icoDeadline);
         require(investors[msg.sender].whitelisted);             
         require(msg.value >= 0.001 ether);   
-        uint amount = msg.value;
-        uint tokensToPurchase = amount * tokensPerWei;
-        require(tokensToPurchase <= tokensForSale - tokensSold);
-        investors[msg.sender].purchasedTokens += tokensToPurchase;      
-        amountRaised += amount;
-        tokensSold += tokensToPurchase;
+        uint256 amount = msg.value;
+        uint256 tokensToPurchase = SafeMath.mul(amount, tokensPerWei);
+        require(tokensToPurchase <= SafeMath.sub(tokensForSale, tokensSold));
+        investors[msg.sender].purchasedTokens = SafeMath.add(investors[msg.sender].purchasedTokens, tokensToPurchase);      
+        amountRaised = SafeMath.add(amountRaised, amount);
+        tokensSold = SafeMath.add(tokensSold, tokensToPurchase);
     }
 
     function setTokensForSale() public {
         require(msg.sender == owner);
-        uint totalTokens = tokenReward.balanceOf(this);
-        tokensForSale = totalTokens;
+        tokensForSale = tokenReward.balanceOf(this);    // tokens owned by this contract
     }
     
     function addToWhitelist(address[] addresses) public {
@@ -109,16 +156,11 @@ contract Crowdsale {
         emit FundTransfer(beneficiary, amountRaised, false);        
     }
 
-    function checkIfIcoEnded() afterIcoDeadline public {
-        require(msg.sender == owner);        
-        crowdsaleClosed = true;
-    }
-
     function burnUnsoldTokens() afterIcoDeadline public {
         require(msg.sender == owner);
         require(!unsoldTokensBurnt);
         unsoldTokensBurnt = true;
-        uint256 unsoldTokens = tokensForSale - tokensSold;
+        uint256 unsoldTokens = SafeMath.sub(tokensForSale, tokensSold);
         require(unsoldTokens > 0);
         tokenReward.burn(unsoldTokens);
         // Todo: Handle return of burn function
@@ -128,7 +170,7 @@ contract Crowdsale {
         require(msg.sender == owner);
         require(!unsoldTokensTransferred);
         unsoldTokensTransferred = true;                
-        uint256 unsoldTokens = tokensForSale - tokensSold;
+        uint256 unsoldTokens = SafeMath.sub(tokensForSale, tokensSold);
         require(unsoldTokens > 0);        
         tokenReward.transfer(toAddress, unsoldTokens);
         emit FundTransfer(toAddress, unsoldTokens, true);        
@@ -148,7 +190,7 @@ contract Crowdsale {
         require(investors[msg.sender].whitelisted);                
         require(investors[msg.sender].purchasedTokens > 0);   
         require(!investors[msg.sender].tokensClaimed);        
-        uint tokens = investors[msg.sender].purchasedTokens;
+        uint256 tokens = investors[msg.sender].purchasedTokens;
         investors[msg.sender].purchasedTokens = 0;
         investors[msg.sender].tokensClaimed = true;
         tokenReward.transfer(msg.sender, tokens);     
