777cb8f9c0d419b7ffb3a284cab07a38dd907c34
Gokhan Simsek <g.simsek@paralect.com>
2018-07-07 20:23:43 +0300

    Adding and using SafeMath in token contract

--- ethereum/crowdsale/ico_sample/z1_TokenERC20.sol
+++ ethereum/crowdsale/ico_sample/z1_TokenERC20.sol
@@ -1,4 +1,28 @@
-pragma solidity ^0.4.16;
+pragma solidity 0.4.21;
+
+/**
+* @title SafeMath by OpenZeppelin
+* @dev Math operations with safety checks that throw on error
+*/
+library SafeMath {
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
@@ -7,7 +31,6 @@ contract TokenERC20 {
     string public name;
     string public symbol;
     uint8 public decimals = 18;
-    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
     // This creates an array with all balances
@@ -45,16 +68,16 @@ contract TokenERC20 {
         // Check if the sender has enough
         require(balanceOf[_from] >= _value);
         // Check for overflows
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        require(SafeMath.add(balanceOf[_to], _value) >= balanceOf[_to]);
         // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        uint previousBalances = SafeMath.add(balanceOf[_from], balanceOf[_to]);
         // Subtract from the sender
-        balanceOf[_from] -= _value;
+        balanceOf[_from] = SafeMath.sub(balanceOf[_from], _value);
         // Add the same to the recipient
-        balanceOf[_to] += _value;
+        balanceOf[_to] = SafeMath.add(balanceOf[_to], _value);
         emit Transfer(_from, _to, _value);
         // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+        assert(SafeMath.add(balanceOf[_from], balanceOf[_to]) == previousBalances);
     }
 
     /**
@@ -80,7 +103,7 @@ contract TokenERC20 {
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
+        allowance[_from][msg.sender] = SafeMath.sub(allowance[_from][msg.sender], _value);
         _transfer(_from, _to, _value);
         return true;
     }
@@ -100,25 +123,6 @@ contract TokenERC20 {
     }
 
     /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-
-    /**
      * Destroy tokens
      *
      * Remove `_value` tokens from the system irreversibly
@@ -127,27 +131,9 @@ contract TokenERC20 {
      */
     function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
+        balanceOf[msg.sender] = SafeMath.sub(balanceOf[msg.sender], _value); // Subtract from the sender
+        totalSupply = SafeMath.sub(totalSupply, _value);                     // Updates totalSupply
         emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        emit Burn(_from, _value);
         return true;
     }
 }
